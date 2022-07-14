1cfe5cab99b288cfbb60e1619c7d1071afa47aab
Renata Kim <renata_kim@hotmail.com>
2022-07-14 14:02:45 -0300

    exercicio Renata

---
+++ Renata/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Aula28.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Aula28.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var turma = new TurmaModel();
+            turma.Alunos = new List<Aluno>()
+            {
+                { new Aluno(){ Id = 1,Nome = "Willian",Sobrenome = "Sant Anna",Status = "Reprovado"}},
+                { new Aluno(){ Id = 2,Nome = "Thiago",Sobrenome = "Machado",Status = "Aprovado"}},
+                { new Aluno(){ Id = 3,Nome = "Rodrigo",Sobrenome = "Brago",Status = "Aprovado"}},
+                { new Aluno(){ Id = 4,Nome = "Caroline",Sobrenome = "da Silva",Status = "Recuperação"}}
+
+            };
+
+            return View(turma);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Renata/Models/TurmaModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula28.Models
+{
+    public class TurmaModel
+    {
+        public List<Aluno> Alunos { get; set; }
+    }
+
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }    
+        public string Sobrenome { get; set; }
+        public string Status { get; set; }
+    }
+}

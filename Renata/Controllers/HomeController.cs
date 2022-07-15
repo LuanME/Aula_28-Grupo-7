0e7ea60c2367b4515c2c93f3255ad89fea0ce423
Renata Kim <renata_kim@hotmail.com>
2022-07-15 17:22:44 -0300

    Exercicio 28 atualizado

--- Renata/Controllers/HomeController.cs
+++ Renata/Controllers/HomeController.cs
@@ -15,19 +15,28 @@ namespace Aula28.Controllers
 
         public IActionResult Index()
         {
-            var turma = new TurmaModel();
-            turma.Alunos = new List<Aluno>()
+            var turma = new AlunosModel();
+            turma.Turma = new List<Aluno>()
             {
                 { new Aluno(){ Id = 1,Nome = "Willian",Sobrenome = "Sant Anna",Status = "Reprovado"}},
                 { new Aluno(){ Id = 2,Nome = "Thiago",Sobrenome = "Machado",Status = "Aprovado"}},
                 { new Aluno(){ Id = 3,Nome = "Rodrigo",Sobrenome = "Brago",Status = "Aprovado"}},
                 { new Aluno(){ Id = 4,Nome = "Caroline",Sobrenome = "da Silva",Status = "Recuperação"}}
-
             };
-
             return View(turma);
         }
-
+        public IActionResult Notas()
+        {
+            var materia = new AlunosModel();
+            materia.Materias = new List<Materia>()
+            {
+                { new Materia(){ Descricao = "Português",PrimeiroSemestre = 5,SegundoSemestre = 6.5,TerceiroSemestre = 6.5, QuartoSemestre = 0, Status = "Reprovado" }},
+                { new Materia(){ Descricao = "Matemática",PrimeiroSemestre = 10,SegundoSemestre = 8,TerceiroSemestre = 8, QuartoSemestre = 8, Status = "Aprovado" }},
+                { new Materia(){ Descricao = "Ciências",PrimeiroSemestre = 6,SegundoSemestre = 7.5,TerceiroSemestre = 8, QuartoSemestre = 8, Status = "Aprovado" }},
+                { new Materia(){ Descricao = "Português",PrimeiroSemestre = 8,SegundoSemestre = 9,TerceiroSemestre = 10, QuartoSemestre = 7.5, Status = "Aprovado" }}
+            };
+            return View(materia);
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Renata/Models/AlunosModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula28.Models
+{
+    public class AlunosModel
+    {
+        public List <Aluno> Turma { get; set; }
+        public List<Materia> Materias { get; set; }
+
+    }
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Status { get; set; }
+    }
+    public class Materia
+    {
+        public string Descricao { get; set; }
+        public double PrimeiroSemestre { get; set; }
+        public double SegundoSemestre { get; set; }
+        public double TerceiroSemestre { get; set; }
+        public double QuartoSemestre { get; set; }
+        public string Status { get; set; }
+
+    }
+}
